ef88e338ee4e5ab4196a5f6958372ccc595c6521
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-08-30 23:39:17 +0200

    Planteando selector de redes sociales al crear usuario

--- resources/views/panel/users/forms/addedit/_details.blade.php
+++ resources/views/panel/users/forms/addedit/_details.blade.php
@@ -1,2 +1,23 @@
--profesion
--web
+<div class="row">
+    {{-- Profesión --}}
+    <div class="col-md-6">
+        <div class="form-group">
+            <label for="profesion">Profesión</label>
+            <input name="profesion"
+                   type="text"
+                   class="form-control"
+                   placeholder="Profesión" />
+        </div>
+    </div>
+
+    {{-- Web --}}
+    <div class="col-md-6">
+        <div class="form-group">
+            <label for="web">Web</label>
+            <input name="web"
+                   type="text"
+                   class="form-control"
+                   placeholder="Web" />
+        </div>
+    </div>
+</div>
--- resources/views/panel/users/forms/addedit/_social.blade.php
+++ resources/views/panel/users/forms/addedit/_social.blade.php
@@ -1,3 +1,58 @@
-- Seleccionar Red Social
-- Nick de red social
-- url de tu perfil
+<div class="row">
+    {{-- Profesión --}}
+    <div class="col-md-4">
+        <div class="form-group">
+            <label for="profesion">Seleccionar Red Social</label>
+            <select title="Select your spell" class="form-control selectpicker">
+                <option data-thumbnail="{{asset('images/icons/social-network/facebook.png')
+                }}">Chrome</option>
+
+                <option data-icon="glyphicon glyphicon-eye-open"
+                        data-subtext="Facebook">
+                    Facebook
+                </option>
+            </select>
+
+            <select class="selectpicker">
+                <option data-thumbnail="http://icons.iconarchive.com/icons/tinylab/android-lollipop-apps/24/Foursquare-icon.png" >Foursquare</option>
+                <option data-thumbnail="http://icons.iconarchive.com/icons/tinylab/android-lollipop-apps/24/Twitter-icon.png">Twitter</option>
+                <option data-thumbnail="http://icons.iconarchive.com/icons/tinylab/android-lollipop-apps/24/Evernote-icon.png">Evernote</option>
+                <option data-thumbnail="http://icons.iconarchive.com/icons/tinylab/android-lollipop-apps/24/Dropbox-icon.png" >Dropbox</option>
+            </select>
+
+            <hr />
+
+            <select class="selectpicker">
+                <option data-icon="glyphicon glyphicon-eye-open" data-subtext="petrification">Eye of Medusa</option>
+                <option data-icon="glyphicon glyphicon-fire" data-subtext="area damage">Rain of Fire</option>
+            </select>
+
+
+
+
+
+        </div>
+    </div>
+
+    {{-- Web --}}
+    <div class="col-md-4">
+        <div class="form-group">
+            <label for="web">Nick en la red social</label>
+            <input name="web"
+                   type="text"
+                   class="form-control"
+                   placeholder="Web" />
+        </div>
+    </div>
+
+    {{-- Web --}}
+    <div class="col-md-4">
+        <div class="form-group">
+            <label for="web">Url de tu perfil</label>
+            <input name="web"
+                   type="text"
+                   class="form-control"
+                   placeholder="Web" />
+        </div>
+    </div>
+</div>
