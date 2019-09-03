b9042260877d8e074d6c3dbcb3cdbcc5eb67d6e2
Raúl Caro Pastorino <dev@fryntiz.es>
2019-09-03 17:34:46 +0200

    password_repeat

--- resources/views/panel/users/forms/addedit/_login.blade.php
+++ resources/views/panel/users/forms/addedit/_login.blade.php
@@ -1,4 +1,58 @@
-- Correo
-- Nick
-- Contraseña
-- Repetir Contraseña
+<div class="row">
+    {{-- Correo --}}
+    <div class="col-md-12">
+        <label for="mail">Correo</label>
+
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="mail">
+                    <i class="fa fa-mail-bulk"></i>
+                </span>
+            </div>
+
+            <input type="email" class="form-control"
+                   placeholder="Correo"
+                   aria-describedby="mail"
+                   name="mail"
+                   required>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    {{-- Contraseña --}}
+    <div class="col-md-6">
+        <label for="password">Password</label>
+
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="password">
+                    <i class="fa fa-user-secret"></i>
+                </span>
+            </div>
+
+            <input type="password" class="form-control"
+                   aria-describedby="password"
+                   name="password"
+                   required />
+        </div>
+    </div>
+
+    {{-- Contraseña --}}
+    <div class="col-md-6">
+        <label for="password_repeat">Password Repeat</label>
+
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="password_repeat">
+                    <i class="fa fa-repeat"></i>
+                </span>
+            </div>
+
+            <input type="password" class="form-control"
+                   aria-describedby="password_repeat"
+                   name="password_repeat"
+                   required />
+        </div>
+    </div>
+</div>
