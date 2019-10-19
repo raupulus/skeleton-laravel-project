d49f4ddaf50c519854f1f8f38fb8f276f2b9af6e
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-10-19 22:41:35 +0200

    Mejoro migraciones de usuario con datos asociados

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -12,22 +13,67 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $table = 'users';
+        $table_users = 'users';
+        $table_configuration = 'users_configuration';
+        $table_data = 'users_data';
+        $table_details = 'users_detail';
+        $timestamp = Carbon::now();
 
-        DB::table($table)->insert([
-            'name' => 'Administrador Principal',
-            'role_id' => 1,
-            'nick' => 'elsuperadmin',
-            'email' => 'admin@domain.es',
-            'password' => bcrypt('temp'),
-        ]);
+        $users = [
+            ## Datos de usuario administrador.
+            [
+                'name' => 'Administrador Principal',
+                'role_id' => 1,
+                'nick' => 'elsuperadmin',
+                'email' => 'admin@domain.es',
+            ],
 
-        DB::table($table)->insert([
-            'name' => 'Usuario Normal',
-            'role_id' => 3,
-            'nick' => 'elusernormal',
-            'email' => 'user@domain.es',
-            'password' => bcrypt('temp'),
-        ]);
+            ## Datos de usuario normal.
+            [
+                'name' => 'Usuario Normal',
+                'role_id' => 3,
+                'nick' => 'elusernormal',
+                'email' => 'user@domain.es',
+                'password' => bcrypt('temp'),
+            ]
+        ];
+
+        /**
+         * Inserto cada usuario creando los datos en las tablas dependientes.
+         */
+        foreach ($users as $user) {
+            $configuration = DB::table($table_configuration)->insertGetId([
+                'send_email' => true,
+                'send_notification' => true,
+                'send_notification_push' => true,
+                'created_at' => $timestamp,
+                'updated_at' => $timestamp,
+            ]);
+
+            $data = DB::table($table_data)->insertGetId([
+                'created_at' => $timestamp,
+                'updated_at' => $timestamp,
+            ]);
+
+            $details = DB::table($table_details)->insertGetId([
+                'profession' => 'Developer',
+                'web' => 'https://fryntiz.es',
+                'created_at' => $timestamp,
+                'updated_at' => $timestamp,
+            ]);
+
+            DB::table($table_users)->insert(
+                array_merge([
+                    'configuration_id' => $configuration,
+                    'data_id' => $data,
+                    'detail_id' => $details,
+                    'password' => bcrypt('123456'),
+                    'remember_token' => true,
+                    'email_verified_at' => $timestamp,
+                    'created_at' => $timestamp,
+                    'updated_at' => $timestamp,
+                ], $user)
+            );
+        }
     }
 }
