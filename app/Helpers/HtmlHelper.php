bec3783ecf9908aaaabb165b5463187dbe0a4dfa
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-11-04 10:40:12 +0100

    Refactorizo Helper html con botones

--- app/Helpers/HtmlHelper.php
+++ app/Helpers/HtmlHelper.php
@@ -9,30 +9,99 @@
 
 class Buttom
 {
+    /**
+     * Devuelve un botón genérico.
+     *
+     * @param       $action URL del controlador que llamará.
+     * @param       $dataName El identificador de tipo "data-name" que tendrá.
+     * @param array $options Opciones posibles:
+     *              class => clases css.
+     *              text => Nombre del botón.
+     *              icon => Clases para el icono.
+     *
+     * @return string
+     */
+    public static function generic($action, $dataName,  $options = [])
+    {
+        $options = array_merge([
+            'class' => 'm-1 btn btn-primary btn-panel btn-panel-generic',
+            'text' => 'Ver',
+            'icon' => 'fa fa-edit',
+        ], $options);
+
+        return ' <a ' . 'data-name="' . $dataName . '" ' .
+            'class="' . $options['class'] . '" ' .
+            'href="' . $action . '">' .
+            '<i class="' . $options['icon'] . '"></i> ' .
+            '<span>' .
+            $options['text'] .
+            '</span>' .
+            '</a> ';
+    }
+
+    /**
+     * Devuelve un botón genérico con un formulario dentro.
+     *
+     * @param       $action URL del controlador que llamará.
+     * @param       $id El id del elemento sobre el que actúa.
+     * @param array $options Opciones posibles:
+     *              class => clases css.
+     *              text => Nombre del botón.
+     *              valueId => Prefijo del id para el formulario.
+     *              icon => Clases css para el icono del botón.
+     *              confirm => Mensaje para confirmar envío.
+     *              method => Tipo de envío para el formulario.
+     *              alert => Indica si muestra cartel para confirmar envío.
+     *              inputs => Campos ocultos extras: [name => value]
+     *
+     * @return string
+     */
     public static function genericForm($action, $id,  $options = [])
     {
         $options = array_merge([
             'class' => 'm-1 btn btn-sm btn-danger delete',
-            'text' => 'Eliminar',
-            'valueId' => 'form-delete',
+            'text' => 'Botón',
+            'valueId' => 'form-generic',
             'icon' => 'fa fa-trash',
-            'confirm' => '¿Estás seguro de eliminar?'
+            'confirm' => '¿Estás seguro de continuar?',
+            'method' => 'POST',
+            'alert' => false,
+            'inputs' => [],  ## Representa todos los campos ocultos
         ], $options);
 
-        return
+        $form = '<form action="' . $action . '" ' .
+            'method="' . $options['method'] . '" ' .
+            'id="' . $options['valueId'] . '-' . $id . '">' .
+            csrf_field() .
+
+            '<input type="hidden" name="id" value="' . $id . '">';
+
+        foreach ($options['inputs'] as $name => $value) {
+            $form .= '<input type="hidden" name="' . $name . '" value="' . $value . '">';
+        }
+
+        $form .= '<button ' .
+            'class="' . $options['class'] . '" ';
+
+        if ($options['alert']) {
+            $form .= 'onclick="formSendConfirm(event, this, \'' . $options['confirm'] . '\')"';
+            $form .= ' type="button"';
+        } else {
+            $form .= ' type="submit"';
+        }
+
+        $form .= '><i class="' . $options['icon'] . '"></i> ' .
+            '<span class="d-none d-md-inline-block">' .
+                $options['text'] .
+            '</span>' .
+        '</button>' .
+        '</form>';
+
+        return $form;
     }
 
     /**
      * Devuelve un botón para eliminar.
-     *
-     * @param       $action URL del controlador que llamará.
-     * @param       $id El id del elemento a eliminar.
-     * @param array $options Opciones posibles:
-     *              class => clases css,
-     *              text => Nombre del botón
-     *              valueId => Prefijo del id para el formulario
-     *
-     * @return string
      */
     public static function delete($action, $id,  $options = [])
     {
@@ -41,68 +110,11 @@ class Buttom
             'text' => 'Eliminar',
             'valueId' => 'form-delete',
             'icon' => 'fa fa-trash',
-            'confirm' => '¿Estás seguro de eliminar?'
+            'confirm' => '¿Estás seguro de eliminar?',
+            'alert' => true,
         ], $options);
 
-        return '<form action="' . $action . '" ' .
-            'method="POST" ' .
-            'id="' . $options['valueId'] . '-' . $id . '">' .
-            csrf_field() .
-
-            '<input type="hidden" name="id" value="' . $id . '">' .
-
-            '<button type="button" ' .
-                     'class="' . $options['class'] . '" ' .
-                     'onclick="formSendConfirm(event, this, \'' . $options['confirm'] . '\')">' .
-                '<i class="' . $options['icon'] . '"></i> ' .
-                '<span class="d-none d-md-inline-block">' .
-                    $options['text'] .
-                '</span>' .
-            '</button>' .
-        '</form>';
-    }
-
-    /**
-     * Devuelve un botón para aprobar.
-     *
-     * @param       $action URL del controlador que llamará.
-     * @param       $id El id del elemento a aprobar.
-     * @param array $options Opciones posibles:
-     *              class => clases css,
-     *              text => Nombre del botón
-     *              valueId => Prefijo del id para el formulario
-     *
-     * @return string
-     */
-    public static function aprobar($action, $id,  $options = [])
-    {
-        if (! array_key_exists('class', $options)) {
-            $options['class'] = 'btn btn-sm btn-success pull-right edit';
-        }
-
-        if (! array_key_exists('text', $options)) {
-            $options['text'] = 'Aprobar';
-        }
-
-        if (! array_key_exists('valueId', $options)) {
-            $options['valueId'] = 'form-aprobar';
-        }
-
-        return '<form action="' . $action . '" ' .
-            'method="POST" ' .
-            'id="' . $options['valueId'] . '-' . $id . '" >' .
-            csrf_field() .
-
-            '<input type="hidden" name="id" value="' . $id . '">' .
-
-            '<button type="submit" ' .
-            'class="' . $options['class'] . '">' .
-            '<i class="fa fa-check"></i> ' .
-            '<span class="hidden-xs hidden-sm">' .
-            $options['text'] .
-            '</span>' .
-            '</button>' .
-            '</form>';
+        return self::genericForm($action, $id, $options);
     }
 
     /**
@@ -226,33 +238,5 @@ class Buttom
         return $button;
     }
 
-    /**
-     * Devuelve un botón para ver.
-     *
-     * @param       $action URL del controlador que llamará.
-     * @param       $dataName El identificador de tipo "data-name" que tendrá.
-     * @param array $options Opciones posibles:
-     *              class => clases css.
-     *              text => Nombre del botón.
-     *              icon => Clases para el icono.
-     *
-     * @return string
-     */
-    public static function generic($action, $dataName,  $options = [])
-    {
-        $options = array_merge([
-            'class' => 'm-1 btn btn-primary btn-panel btn-panel-generic',
-            'text' => 'Ver',
-            'icon' => 'fa fa-edit',
-        ], $options);
 
-        return ' <a ' . 'data-name="' . $dataName . '" ' .
-            'class="' . $options['class'] . '" ' .
-            'href="' . $action . '">' .
-            '<i class="' . $options['icon'] . '"></i> ' .
-            '<span>' .
-                $options['text'] .
-            '</span>' .
-            '</a> ';
-    }
 }
