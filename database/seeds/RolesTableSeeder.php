07cd34b0f38d2dc9452e43e0e92aeb54f1a2d397
Raúl Caro Pastorino <tecnico@fryntiz.es>
2019-11-04 10:39:10 +0100

    Añado descripción al crear roles

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -19,18 +19,21 @@ class RolesTableSeeder extends Seeder
         DB::table($table)->insert([
             'name' => 'superadmin',
             'display_name' => 'SuperAdministrador',
+            'description' => 'Administrador principal de la aplicación'
         ]);
 
         ## Creo rol para los demás administradores.
         DB::table($table)->insert([
             'name' => 'admin',
             'display_name' => 'Administrador',
+            'description' => 'Administrador con poderes amplios pero no totales'
         ]);
 
         ## Creo rol para usuario normal
         DB::table($table)->insert([
             'name' => 'user',
             'display_name' => 'Usuario Normal',
+            'description' => 'Usuario que puede acceder al contenido y editar el suyo'
         ]);
     }
 }
