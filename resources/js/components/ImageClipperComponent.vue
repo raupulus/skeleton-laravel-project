eb05fdfb8c2feae826c9d31dd34dd6174ba9ec2d
Raúl Caro Pastorino <dev@fryntiz.es>
2019-11-20 18:19:17 +0100

    Avanzando componente para cortar imágenes

--- resources/js/components/ImageClipperComponent.vue
+++ resources/js/components/ImageClipperComponent.vue
@@ -3,27 +3,42 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Título test</div>
+                    <div class="card-header">
+                        <h3>Upload Image</h3>
+                    </div>
 
                     <div class="card-body">
-                        Subtítulo test
+                        <h4>Image upload</h4>
                         <br />
                         Mensaje: {{msg}}
 
                         <br />
+
+
                         <button>
                             <clipper-upload v-model="imgURL">upload image</clipper-upload>
                         </button>
-                        <button @click="getResult">clip image</button>
-                        <clipper-basic class="my-clipper" ref="clipper" :src="imgURL" preview="my-preview">
+
+                        <button @click="getResult">
+                            clip image
+                        </button>
+
+                        <clipper-basic class="my-clipper"
+                                       ref="clipper"
+                                       :src="imgURL"
+                                       preview="my-preview">
                             <div class="placeholder" slot="placeholder">No image</div>
                         </clipper-basic>
+
                         <div>
                             <div>preview:</div>
                             <clipper-preview name="my-preview" class="my-clipper">
-                                <div class="placeholder" slot="placeholder">preview area</div>
+                                <div class="placeholder" slot="placeholder">
+                                    preview area
+                                </div>
                             </clipper-preview>
                         </div>
+
                         <div>
                             <div>result:</div>
                             <img class="result" :src="resultURL" alt="">
@@ -40,8 +55,9 @@
     //https://timtnleeproject.github.io/vuejs-clipper/#/examples/quick-start
     //https://timtnleeproject.github.io/vuejs-clipper/#/examples/profile-photo
 
-    import VuejsClipper from 'vuejs-clipper';
-    Vue.use(VuejsClipper);
+    //import VuejsClipper from 'vuejs-clipper';
+    import clipperFixed from 'vuejs-clipper';
+    Vue.use(clipperFixed);
 
     export default {
         //name: 'v-image-clipper',
