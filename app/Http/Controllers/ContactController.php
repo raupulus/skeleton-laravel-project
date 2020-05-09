a96bb2deb0392de7a153cf1882360aa7568a1c30
Raúl Caro Pastorino <dev@fryntiz.es>
2020-05-10 00:02:04 +0200

    Reconfiguro captcha con el paquete oficial de google por quedar sin soporte el usado anterioremente

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -13,6 +13,7 @@ use function config;
 use Exception;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
+use function dd;
 use function env;
 use function view;
 
@@ -41,16 +42,29 @@ class ContactController extends Controller
      */
     public function send(ContactRequest $request)
     {
-        ## Validación del captcha
-        $score = 0;
-        //$score = ReCaptcha::verify($request->get('g-recaptcha-response'), 'contact');
 
-        /*
-        if ($score > 0.7) {
-            // Perfecto
-        } elseif($score > 0.3) {
-            // TODO → Pedir confirmación adicional por email antes de enviar
-        } else {
+        // TODO → Guardar estos emails marcados como spam en la DB
+        // TODO → Pedir validación por email antes de enviar este correo
+
+        ## Validación del captcha
+        $googleRecaptcha = (new \ReCaptcha\ReCaptcha(config('constant.google_recaptcha_secret')))
+            ->setExpectedAction('contact')
+            ->verify($request->get('g_recaptcha'), $request->ip());
+
+        if (!$googleRecaptcha->isSuccess()) {
+            return view('contact.view')->with([
+                'message' => [
+                    'danger' => [
+                        'No se ha validado el captcha correctamente'
+                    ],
+                ],
+            ]);
+        }
+
+        ## Almaceno la puntuación que le da google recaptcha v3
+        $score = $googleRecaptcha->getScore();
+
+        if ($score < 0.3) {
             return view('contact.view')->with([
                 'message' => [
                     'danger' => [
@@ -59,31 +73,6 @@ class ContactController extends Controller
                 ],
             ]);
         }
-        */
-
-        // NUEVO RECAPTCHA
-
-        //https://medium.com/swlh/laravel-recaptcha-v3-the-easy-way-1e395083201b
-        /*
-        $googleRecaptcha = (new \ReCaptcha\ReCaptcha(config('constant.google_recaptcha_secret')))
-            ->setExpectedAction('contact_form')
-            ->verify($request->input('_recaptcha'), $request->ip());
-
-        if (!$response->isSuccess()) {
-            abort();
-        }    if ($response->getScore() < 0.6) {
-        return response()->view('challenge');
-           }
-        */
-
-
-
-
-
-        $validar = Validator::make($request->all(), [
-            ## Valido el score de la petición con puntuación mínima de 0.3
-            //'g-recaptcha-response' => 'required|recaptchav3:contact,0.3'
-        ]);
 
         $data = [
             'name' => $request->get('name'),
