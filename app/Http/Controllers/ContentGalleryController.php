5340f5284f090a208f572742b9526a2e075b787c
Raúl Caro Pastorino <dev@fryntiz.es>
2020-05-10 14:22:25 +0200

    Creo Modelo, Controlador, Migración, Seeder como base sin rellenar datos para ContentGallery

---
+++ app/Http/Controllers/ContentGalleryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ContentGallery;
+use Illuminate\Http\Request;
+
+class ContentGalleryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ContentGallery  $contentGallery
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ContentGallery $contentGallery)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ContentGallery  $contentGallery
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ContentGallery $contentGallery)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ContentGallery  $contentGallery
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ContentGallery $contentGallery)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ContentGallery  $contentGallery
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ContentGallery $contentGallery)
+    {
+        //
+    }
+}
---
+++ database/factories/ContentGalleryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ContentGallery;
+use Faker\Generator as Faker;
+
+$factory->define(ContentGallery::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
