a37b9a5d66b2b6f0ba3bd1692a3a7a3cc2e71694
Raúl Caro Pastorino <dev@fryntiz.es>
2020-05-10 17:47:45 +0200

    Preparo migración para los tipos de contenidos

--- database/migrations/2020_05_10_115305_create_content_types_table.php
+++ database/migrations/2020_05_10_115305_create_content_types_table.php
@@ -18,6 +18,26 @@ class CreateContentTypesTable extends Migration
             $table->charset = 'utf8';
             $table->collation = 'utf8_unicode_ci';
             $table->bigIncrements('id');
+            $table->bigInteger('file_id')
+                ->nullable()
+                ->comment('FK a la imagen en la tabla files');
+            $table->foreign('file_id')
+                ->references('id')->on('files')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+            $table->string('name', 511)
+                ->unique()
+                ->comment('Nombre de la categoría');
+            $table->string('slug', 255)
+                ->unique()
+                ->comment('Slug para el URL');
+            $table->string('description', 1023)
+                ->nullable()
+                ->comment('Descripción acerca de lo que contendrá esta categoría');
+            $table->string('icon', 255)->nullable()->comment('Clase css para el icono');
+            $table->string('color', 255)
+                ->default('#000000')
+                ->comment('Código Hexadecimal del color');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -30,6 +50,9 @@ class CreateContentTypesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('content_types');
+        Schema::dropIfExists('content_types', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+            $table->dropForeign(['file_id']);
+        });
     }
 }
