b8c5cdc615a842c65dc477a2af006ca0e2585fcf
Raúl Caro Pastorino <dev@fryntiz.es>
2020-05-10 14:19:54 +0200

    Creo Modelo, Controlador, Migración, Seeder como base sin rellenar datos para ContentContributor

---
+++ database/factories/ContentContributorFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ContentContributor;
+use Faker\Generator as Faker;
+
+$factory->define(ContentContributor::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
