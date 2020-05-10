5b39736d2d6bcc1cba81d042c12799926e88a982
Raúl Caro Pastorino <dev@fryntiz.es>
2020-05-10 17:26:25 +0200

    Preparo migración para subcategorías

--- database/migrations/2020_05_10_115246_create_subcategories_table.php
+++ database/migrations/2020_05_10_115246_create_subcategories_table.php
@@ -18,6 +18,33 @@ class CreateSubcategoriesTable extends Migration
             $table->charset = 'utf8';
             $table->collation = 'utf8_unicode_ci';
             $table->bigIncrements('id');
+            $table->bigInteger('category_id')
+                ->nullable()
+                ->comment('FK a la categoría principal');
+            $table->foreign('category_id')
+                ->references('id')->on('categories')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+            $table->bigInteger('file_id')
+                ->nullable()
+                ->comment('FK a la imagen en la tabla files');
+            $table->foreign('file_id')
+                ->references('id')->on('files')
+                ->onUpdate('cascade')
+                ->onDelete('set null');
+            $table->string('name', 511)
+                ->unique()
+                ->comment('Nombre de la categoría');
+            $table->string('slug', 255)
+                ->unique()
+                ->comment('Slug para el URL');
+            $table->string('description', 1023)
+                ->nullable()
+                ->comment('Descripción acerca de lo que contendrá esta categoría');
+            $table->string('icon', 255)->nullable()->comment('Clase css para el icono');
+            $table->string('color', 255)
+                ->default('#000000')
+                ->comment('Código Hexadecimal del color');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -30,6 +57,9 @@ class CreateSubcategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('subcategories');
+        Schema::dropIfExists('subcategories', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+            $table->dropForeign(['file_id']);
+        });
     }
 }
