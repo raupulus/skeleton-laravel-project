802557992519bf9c74d5d7328a88962d7a833f98
Raúl Caro Pastorino <dev@fryntiz.es>
2020-05-10 14:17:00 +0200

    Creo Modelo, Controlador, Migración, Seeder como base sin rellenar datos para ContentSubcategory

---
+++ database/factories/ContentSubcategoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ContentSubcategory;
+use Faker\Generator as Faker;
+
+$factory->define(ContentSubcategory::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
