e122ab6ce4fc913045e2fe39eca427c138906533
Raúl Caro Pastorino <dev@fryntiz.es>
2020-05-12 20:03:52 +0200

    Doy cuerpo a la migración del contenido asociado a la galería

--- database/migrations/2020_05_10_122153_create_content_galleries_table.php
+++ database/migrations/2020_05_10_122153_create_content_galleries_table.php
@@ -14,12 +14,25 @@ class CreateContentGalleriesTable extends Migration
     public function up()
     {
         Schema::create('content_galleries', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
-            $table->charset = 'utf8';
-            $table->collation = 'utf8_unicode_ci';
-            $table->bigIncrements('id');
+            $table->bigInteger('gallery_id')
+                ->nullable()
+                ->comment('FK al a la galería que pertenezca');
+            $table->foreign('gallery_id')
+                ->references('id')->on('galleries')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->bigInteger('content_id')
+                ->nullable()
+                ->comment('FK al contenido que se asocia con la galería');
+            $table->foreign('content_id')
+                ->references('id')->on('contents')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
             $table->timestamps();
             $table->softDeletes();
+
+            $table->unique(['gallery_id', 'content_id']);
+            $table->index(['gallery_id', 'content_id']);
         });
     }
 
@@ -30,6 +43,9 @@ class CreateContentGalleriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('content_galleries');
+        Schema::dropIfExists('content_galleries', function (Blueprint $table) {
+            $table->dropForeign(['gallery_id']);
+            $table->dropForeign(['content_id']);
+        });
     }
 }
